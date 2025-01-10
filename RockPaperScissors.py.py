f8683092789f82fa8b9bb754764b3c3aa17e3508
zchengzhu <zchengzhu@cps.edu>
2025-01-10 14:21:21 -0600

    RockPaperScissors.py.py

--- RockPaperScissors.py.py
+++ RockPaperScissors.py.py
@@ -7,8 +7,6 @@ import random
 #Functions
 #main
 
-#Step 1: Player selects an option
-#Step 2: Making a selection for the computer
 computer = 0
 player = "0"
 
@@ -16,36 +14,41 @@ def RPS():
     global computer
     global player
     print("Welcome to Rock Paper Scisssors..!")
-    print("Select Rock, Paper or scissors..")
+    print("Select Rock, Paper or scissors..") #Player(You) select one of these three options.
     computer = random.randint(1, 3)
-    player = input("Selection: ") #String
-    if computer == 1:
+    player = input("Selection: ") #String #Tells the program what the player(You) have selected.
+    if computer == 1: #Computer options 
         computer = ("rock")
     print("Computer chosen rock..")
-if computer == 2:
+if computer == 2: #Computer options 
     computer == ("paper")
     print("Computer chosen paper..")
-elif computer == 3:
+elif computer == 3: #Computer options 
     computer == ("scissors")
     print("Computer chosen scissors..")
-if computer == "scissors" and player == "paper":
+if computer == "scissors" and player == "paper": #System outcome if option was chosen
     print("You lose..!")
-if computer == "scissors" and player == "scissors":
+if computer == "scissors" and player == "scissors": #System outcome if option was chosen
     print("You tied.. Try again!")
-if computer == "rock" and player == "scissors":
+if computer == "rock" and player == "scissors": #System outcome if option was chosen
     print("You lose..!")
-if computer == "rock" and player == "rock":
+if computer == "rock" and player == "rock": #System outcome if option was chosen
     print("You tied.. Try again!")
-if computer == "paper" and player == "scissors":
+if computer == "paper" and player == "scissors": #System outcome if option was chosen
     print("You lose..!")
-if computer == "paper" and player == "paper":
+if computer == "paper" and player == "paper": #System outcome if option was chosen
     print("You tied.. Try again!")
-if computer == "paper" and player == "scissors":
+if computer == "paper" and player == "scissors": #System outcome if option was chosen
     print("You win..!")
-if computer == "scissors" and player == "rock":
+if computer == "scissors" and player == "rock": #System outcome if option was chosen
     print("You win..!")
-if computer == "rock" and player == "scissors":
+if computer == "rock" and player == "scissors": #System outcome if option was chosen
     print("You win..!")
-
-#Step 3: Determine the outcome
+    choice = input("Do you want to play again..?") #System will ask if you want to play again or not, if yes then game would repeat, if no then game will shut down
+    if choice == "yes":
+        print("Restarting...") #If yes game would repeat
+        RPS()
+    if choice == "no":
+        break #If no then game would end
+        print("Thanks for playing!")
 RPS()
